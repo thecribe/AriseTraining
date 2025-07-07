a7cf7486a4f7d83b17716ba56d30b97a288fd730
Lawal Oluwatobi <lawaloluwatobi128@gmail.com>
2025-07-07 07:22:54 +0100

    Delete whitecloud directory

--- whitecloud/src/app/dashboard/[admin_id]/page.js
+++
@@ -1,9 +0,0 @@
-import SidebarMenuList from "@/components/SidebarMenuList";
-import React from "react";
-import { Fragment } from "react";
-
-const page = () => {
-  return <Fragment>Dashboard</Fragment>;
-};
-
-export default page;
