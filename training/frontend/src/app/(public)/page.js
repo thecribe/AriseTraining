cbd57d4c067c1494768c0168e70a8e35282fde4d
Lawal Oluwatobi <lawaloluwatobi128@gmail.com>
2025-07-07 07:20:29 +0100

    first commit

---
+++ training/frontend/src/app/(public)/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Cover from "./UI/Cover";
+
+const page = () => {
+  return (
+    <section>
+      <Cover />
+    </section>
+  );
+};
+
+export default page;
---
+++ training/frontend/src/components/Button.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Button = ({ type, children }) => {
+  let buttonOutput;
+  switch (type) {
+    case "primary":
+      return (buttonOutput = (
+        <button className="primary-button">{children}</button>
+      ));
+      break;
+
+    default:
+      break;
+  }
+
+  return buttonOutput;
+};
+
+export default Button;
